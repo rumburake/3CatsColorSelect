5cfee22d140de42e6911c8df82681904926480ab
RumburaK <rumburake@gmail.com>
2016-11-27 02:55:20 +0000

    lifecycle management with DialogFragment instead of deprecated Dialog

--- app/src/main/java/com/threecats/colorselecttest/MainActivity.java
+++ app/src/main/java/com/threecats/colorselecttest/MainActivity.java
@@ -1,32 +1,45 @@
 package com.threecats.colorselecttest;
 
-import android.app.Dialog;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.threecats.colorselect.ColorSelectDialog;
+import com.threecats.colorselect.ColorSelect;
 
-public class MainActivity extends AppCompatActivity implements ColorSelectDialog.OnColorSelectListener {
+public class MainActivity extends AppCompatActivity implements ColorSelect.ColorSelectListener {
     Button mColorButton;
     int mColor = Color.RED;
+    static final String state_color = "color";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (savedInstanceState != null) {
+            mColor = savedInstanceState.getInt(state_color, Color.RED);
+        }
+
         setContentView(R.layout.activity_main);
 
         mColorButton = (Button)findViewById(R.id.button);
         mColorButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Dialog d = new ColorSelectDialog(MainActivity.this, mColor, MainActivity.this);
-                d.show();
+                ColorSelect newFrag = ColorSelect.newInstance(mColor);
+                newFrag.show(getSupportFragmentManager(), "colorSelect");
             }
         });
+
         updateColorButton();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt(state_color, mColor);
+
+        super.onSaveInstanceState(outState);
     }
 
     @Override
